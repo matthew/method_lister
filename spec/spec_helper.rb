c11633838f068ec93a577ceabbb8e7787b3004a0
Matthew O'Connor <matthew@pinnacle.lan>
2008-11-19 21:48:04 -0800

    load all files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'spec'
 ROOT_DIR = File.dirname(__FILE__)
 $: << File.expand_path("#{ROOT_DIR}/../lib")
 
+require 'method_lister'
+
 Spec::Runner.configure do |config|
   config.mock_with :rr
 end
